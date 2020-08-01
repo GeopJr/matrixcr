2154e295687d6ebfd630010f845613b4bb86877b
GeopJr <geopjr@geopjr.xyz>
2020-08-01 04:33:41 +0300

    :sparkles: Add header builder

--- src/matrixcr/client.cr
+++ src/matrixcr/client.cr
@@ -12,18 +12,19 @@ module Matrix
       @homeserver = URI.parse(@homeserver.not_nil!).host
       @client = HTTP::Client.new(@homeserver.not_nil!)
       if @access_token.nil?
-        response = @client.post("/_matrix/client/r0/login", headers: HTTP::Headers{"User-Agent" => "matrixcr"}, body: {"type" => "m.login.password", "user" => "#{@user}", "password" => "#{@password}"}.to_json)
+        body = {"type" => "m.login.password", "user" => "#{@user}", "password" => "#{@password}"}
+        response = @client.post("/_matrix/client/r0/login", headers: HTTP::Headers{"User-Agent" => "matrixcr v#{Matrix::VERSION}"}, body: body.to_json)
         value = JSON.parse(response.body)
         raise ArgumentError.new(value["error"].to_s) if value["error"]?
         @access_token = value["access_token"].to_s
       end
     end
 
-    private def default_headers
+    private def default_headers(params : Hash(String, String)? = Hash(String, String).new)
       return HTTP::Headers{
         "Authorization" => "Bearer #{@access_token}",
         "User-Agent":      "matrixcr v#{Matrix::VERSION}",
-      }
+      }.merge!(params)
     end
   end
 end
